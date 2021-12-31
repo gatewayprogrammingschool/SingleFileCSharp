3f011d0c2987aa87344743e0585fb2185f3ab818
The Sharp Ninja <ninja@thesharp.ninja>
2021-12-30 18:47:15 -0600

    Expanded 1 files.

--- SfcsTemplate/src/split/split.cs
+++ SfcsTemplate/src/split/split.cs
@@ -13,4 +13,42 @@ var mdFiles = Directory.GetFiles(wd, "*.md");
 
 Console.WriteLine(string.Join(Environment.NewLine, mdFiles));
 
+foreach (var filename in mdFiles)
+{
+    var sb = new StringBuilder();
+
+    using var reader = new StreamReader(System.IO.File.OpenRead(filename));
+
+    var fi = new FileInfo(reader);
+    var basename = fi.Name[0..(fi.Name.LastIndexOf('.'))];
+    var pageNumber = 0;
+    var currentName = $"{basename}-{++pageNumber}.md";
+
+    while (!reader.EndOfStream)
+    {
+        var line = reader.ReadLine();
+
+        if (line.StartsWith("# "))
+        {
+            WriteFile(sb.ToString(), currentName);
+            sb.Clear();
+            currentName = $"{basename}-{++pageNumber}.md";
+        }
+
+        sb.AppendLine(line);
+    }
+
+    WriteFile(sb.ToString(), currentName);
+}
+
+static void WriteFile(string text, string fileName)
+{
+    if (sb.Length > 0)
+    {
+        FileInfo.WriteAllText(fileName, text);
+
+        Console.WriteLine($"Saved: {fileName}");
+    }
+}
+
 Console.WriteLine("Done");
